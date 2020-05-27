d429263a014d42c055b5c40ef7e66effdff6e25c
Fergus Yip <fergus.yipwailam@gmail.com>
2020-05-27 14:31:16 +1000

    update setup file

--- setup.py
+++ setup.py
@@ -11,9 +11,18 @@ OPTIONS = {
     'packages': ['rumps'],
 }
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     app=APP,
     name='DrinkMore',
+    author='Fergus Yip',
+    author_email='fergus.yipwailam@gmail.com',
+    url='https://github.com/FergusYip/DrinkMoreApp',
+    description=
+    'DrinkMore is a Python 3 app for Mac to remind you to drink more water.',
+    long_description=long_description,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
